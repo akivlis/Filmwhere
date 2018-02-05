46dad028929169218c2921ef30e65fd879f19814
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-02-05 22:02:59 +0100

    Initial Commit

---
+++ MovieWanderer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MovieWanderer
+//
+//  Created by Silvia Kuzmova on 05/02/2018.
+//  Copyright © 2018 Silvia Kuzmova. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
