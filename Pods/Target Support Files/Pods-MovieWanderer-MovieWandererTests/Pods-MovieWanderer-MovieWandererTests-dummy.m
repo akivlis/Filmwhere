8aaebe3a5b49cedbb1e16f5f45ec9ef7ba01e42d
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-07-23 20:17:11 +0200

    Remove ignored files

--- Pods/Target Support Files/Pods-MovieWanderer-MovieWandererTests/Pods-MovieWanderer-MovieWandererTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MovieWanderer_MovieWandererTests : NSObject
-@end
-@implementation PodsDummy_Pods_MovieWanderer_MovieWandererTests
-@end
--- Pods/Target Support Files/Pods-MovieWanderer/Pods-MovieWanderer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MovieWanderer : NSObject
-@end
-@implementation PodsDummy_Pods_MovieWanderer
-@end
