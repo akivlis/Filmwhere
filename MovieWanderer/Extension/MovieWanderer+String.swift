2b8e7c29c1438cff46899f2e2c517b097c329af9
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-11-02 10:07:08 +0800

    add localized, validEmail, validPassword to string extension

--- MovieWanderer/Extension/MovieWanderer+String.swift
+++ MovieWanderer/Extension/MovieWanderer+String.swift
@@ -15,4 +15,23 @@ import Foundation
     var utf8Encoded: Data {
         return data(using: .utf8)!
     }
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+    func isValidEmail() -> Bool {
+        if count == 0 {
+            return false
+        }
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
+    }
+    
+    func isValidPassword() -> Bool {
+        if count >= 6 {
+            return true
+        }
+        return false
+    }
 }
