96bbd63f2685aa5203e742ca1739c8fe1456d4a4
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-11-07 16:50:53 +0800

    set prefered font sizes in movieHeaderView

--- MovieWanderer/TabBar/MoviesTab/MovieDetail/MovieHeader/MovieHeaderView.swift
+++ MovieWanderer/TabBar/MoviesTab/MovieDetail/MovieHeader/MovieHeaderView.swift
@@ -31,7 +31,8 @@ class MovieHeaderView: UICollectionReusableView {
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 3
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.sizeToFit()
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
         label.textColor = .gray
         label.contentMode = .top
         return label
@@ -39,7 +40,8 @@ class MovieHeaderView: UICollectionReusableView {
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.textColor = .myDarkGray
         return label
     }()
@@ -48,7 +50,7 @@ class MovieHeaderView: UICollectionReusableView {
         let button = UIButton(type: .system)
         button.setTitle("More", for: .normal)
         button.setTitleColor(.myRed, for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
         button.titleLabel?.textAlignment = .left
         return button
     }()
@@ -61,9 +63,20 @@ class MovieHeaderView: UICollectionReusableView {
         return stackView
     }()
     
+    private lazy var goToMapButton: UIButton = {
+        let goToMapButton =   UIButton(type: .system)
+        goToMapButton.setTitle("Show on map", for: .normal)
+        goToMapButton.tintColor = .white
+        goToMapButton.backgroundColor = .myRed
+        goToMapButton.titleLabel?.font =  UIFont.preferredFont(forTextStyle: .subheadline)
+        goToMapButton.titleEdgeInsets =  UIEdgeInsets(top: 0, left: 12, bottom: 0, right: -12)
+        goToMapButton.contentEdgeInsets = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 24)
+        goToMapButton.layer.cornerRadius = 4
+        return goToMapButton
+    }()
+    
     private var gradient = CAGradientLayer()
     private lazy var photoContainerView = UIView()
-    private let goToMapButton = UIButton(type: .system)
     
     private var didSetConstraints = false
     private var containerHeightLayoutConstraint: Constraint?
@@ -138,16 +151,8 @@ private extension MovieHeaderView {
         photoContainerView.addSubview(moviePhoto)
         photoContainerView.clipsToBounds = true
         addSubview(photoContainerView)
-        
-        goToMapButton.setTitle("Show on map", for: .normal)
-        goToMapButton.tintColor = .white
-        goToMapButton.backgroundColor = .myRed
-        goToMapButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-        goToMapButton.titleEdgeInsets =  UIEdgeInsets(top: 0, left: 12, bottom: 0, right: -12)
-        goToMapButton.contentEdgeInsets = UIEdgeInsets(top: 6, left: 0, bottom: 6, right: 24)
-        goToMapButton.layer.cornerRadius = 4
+    
         addSubview(goToMapButton)
-
         addSubview(titleLabel)
         
         stackView.addArrangedSubview(descriptionLabel)
@@ -183,7 +188,7 @@ private extension MovieHeaderView {
         }
 
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(photoContainerView.snp.bottom).offset(14)
+            make.top.equalTo(goToMapButton.snp.bottom).offset(10)
             make.leading.trailing.equalToSuperview().inset(padding)
         }
         titleLabel.setContentHuggingPriority(.defaultHigh, for: .vertical)
