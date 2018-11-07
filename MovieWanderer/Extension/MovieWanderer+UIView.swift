e88641f1623c70e95a88daa8372ee12425616ca4
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-11-07 16:11:49 +0800

    add method for calculated if the label is truncated

--- MovieWanderer/Extension/MovieWanderer+UIView.swift
+++ MovieWanderer/Extension/MovieWanderer+UIView.swift
@@ -83,6 +83,20 @@ extension UILabel {
         
         self.attributedText = mutableAttributedString
     }
+    
+    func isTruncated() -> Bool {
+        guard let labelText = text else {
+            return false
+        }
+        
+        let labelTextSize = (labelText as NSString).boundingRect(
+            with: CGSize(width: frame.size.width, height: .greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [.font: font],
+            context: nil).size
+
+        return labelTextSize.height > bounds.size.height
+    }
 }
 
 extension UIApplication {
