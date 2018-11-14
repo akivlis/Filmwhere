f3b99a058e0d47615e4c774addb4be2547dadb46
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2018-11-14 11:55:03 +0800

    remove switch map list from localisation tab

--- MovieWanderer/TabBar/LocalizationTab/LocalizationViewController.swift
+++ MovieWanderer/TabBar/LocalizationTab/LocalizationViewController.swift
@@ -14,68 +14,31 @@ import SnapKit
 
 class LocalizationViewController: UIViewController {
     
-    let _view = LocalizationView()
     let disposeBag = DisposeBag()
-    
-    var sceneView : VerticalScenesView!
     var mapView: MapAndScenesCarouselView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(_view)
-        _view.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        
         setupViews()
-        
-        _view.switchControlValueChanged$
-            .subscribe(onNext: { [unowned self] index in
-                self.displayViewAt(index)
-            }).disposed(by: disposeBag)
     }
 }
 
 private extension LocalizationViewController {
     
     private func setupViews() {
-        
+      
         let scenes : [Scene] = [Scene(title: "Jamie", description: "Hahaha", latitude: 48.225660, longitude: 16.399509),
                                 Scene(title: "Lokrum", description: "Hihihhi", latitude: 48.228176, longitude: 16.395046),
                                 Scene(title: "Stairs", description: "Bla bla bla", latitude: 48.206959, longitude: 16.390454),
                                 Scene(title: "Dany", description: "Bla bla bla", latitude:  48.157614, longitude: 17.075666)]
         
         let movie = Movie(title: "", description: "", scenes: scenes, imageUrl: "")
-        
-        sceneView = VerticalScenesView(movie: movie, showHeader: false)
         mapView = MapAndScenesCarouselView(scenes: scenes)
         
-        _view.containerView.addSubview(sceneView)
-        _view.containerView.addSubview(mapView)
-
-        
-        sceneView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
+        view.addSubview(mapView)
         mapView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.leading.trailing.top.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
     }
-    
-    private func displayViewAt(_ index: Int) {
-        
-        switch index {
-        case 0:
-            _view.containerView.bringSubview(toFront: mapView)
-        case 1:
-            _view.containerView.bringSubview(toFront: sceneView)
-        default:
-            break
-        }
-        
-    }
-    
-  
 }
