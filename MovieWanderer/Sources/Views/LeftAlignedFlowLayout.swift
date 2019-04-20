7f20f6a5ea6b9e619716f1f9cedbc4cdd26b8a49
Silvia Kuzmova <silvia.kuzmova21@gmail.com>
2019-04-20 20:26:06 +0700

    remove commented code

--- MovieWanderer/Sources/Views/LeftAlignedFlowLayout.swift
+++ MovieWanderer/Sources/Views/LeftAlignedFlowLayout.swift
@@ -19,32 +19,10 @@ struct Constants {
 
 class LeftAlignedFlowLayout: UICollectionViewFlowLayout {
     
-//    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-//
-//        let spacing = self.minimumLineSpacing
-//        let cellWidth : CGFloat = Constants.ScenesCollection.cellWidth // TODO: make this generic
-//        let index = proposedContentOffset.x / cellWidth
-//
-//        let roundedIndex = abs(index.rounded())
-//        print(roundedIndex)
-
-//        let lastIndex: CGFloat = 5
-//        var x = roundedIndex * (cellWidth + spacing)
-//
-////        if roundedIndex == lastIndex {
-//////            x = ((lastIndex * cellWidth) + UIScreen.main.bounds.width * 0.25) - spacing
-////            x = x + 1000
-////        }
-//        return CGPoint(x: x ,y: proposedContentOffset.y)
-//    }
-    
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
 
-        // Page width used for estimating and calculating paging.
-//        let pageWidth = self.itemSize.width + self.minimumLineSpacing
+        // TODO: this should use itemSize.width
         let pageWidth = Constants.ScenesCollection.cellWidth + self.minimumLineSpacing
-
-        // Make an estimation of the current page position.
         let approximatePage = proposedContentOffset.x / pageWidth
 
         // Determine the current page based on velocity.
@@ -52,15 +30,18 @@ class LeftAlignedFlowLayout: UICollectionViewFlowLayout {
 
         // Create custom flickVelocity.
         let flickVelocity = velocity.x * 0.3
-
+        
         // Check how many pages the user flicked, if <= 1 then flickedPages should return 0.
         let flickedPages = (abs(round(flickVelocity)) <= 1) ? 0 : round(flickVelocity)
 
         // Calculate newHorizontalOffset.
-        let newHorizontalOffset = ((currentPage + flickedPages) * pageWidth) - self.collectionView!.contentInset.left
+        let newHorizontalOffset = ((currentPage + flickedPages) * pageWidth) //- self.collectionView!.contentInset.left
+//
+//        let lastIndex: CGFloat = 5
+//        if lastIndex == 5 {
+//            return proposedContentOffset
+//        }
 
         return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
     }
-    
-    
 }
